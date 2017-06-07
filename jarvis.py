d4beb3362b192d6307315cfa956c742c95ba8b5f
Kim Sungrae <ksr8179@naver.com>
2017-06-08 02:32:30 +0900

    최종 구현 소스
    이 소스는 기존에 사용하려던 Google Cloud 기계 학습을 이용한 Speech API의 사용에 한계를 느껴 다른 방안을 찾던 중 Speech_recognition이라는 라이브러리를 발견하여 지속적인 실행이 되고 여러 항목을 추가하여 "날씨"라고 말할 경우 날씨를 알려주고 LCD화면에 시간과 날씨를 알려준다. "안녕"이라고 말할 경우 안녕하세요 주인님이라는 음성을 말해준다. 또한 "시간"이라고 이야기하면 현재 시간을 알려준다. 이처럼 여러가지 경우를 만들어 적용하면 간단하게 개인 컴퓨터 비서를 얻을 수 있을 것으로 예상된다.

---
+++ jarvis.py
@@ -0,0 +1,91 @@
+#_*_coding: utf-8 _*_
+#!/usr/bin/env python
+# Requires PyAudio and PySpeech.
+
+import sys
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
+#speech recognition
+import speech_recognition as sr
+from time import ctime
+import time
+import os
+from gtts import gTTS
+
+#lcd
+import json
+import requests
+from Adafruit_CharLCD import Adafruit_CharLCD
+from time import sleep
+
+
+params = {"version": "1", "city":"부산", "county":"부산진구","village":"가야1동"}
+headers = {"appKey": "0b7e8caa-3d84-3f65-89b5-eb05b4b31070"}
+r = requests.get("http://apis.skplanetx.com/weather/current/hourly", params=params, headers=headers)
+
+data = r.json()
+
+weather = data["weather"]["hourly"]
+cTime = weather[0]["timeRelease"]
+cSky = weather[0]["sky"]["name"]
+cWind = weather[0]["wind"]["wspd"]
+cTemp = weather[0]["temperature"]["tc"]
+
+cWeather1 = "      Time\n"+cTime
+cWeather2 = "      Temp\n    " + cTemp +"'C"
+lcd = Adafruit_CharLCD(rs=22, en=11, d4=23, d5=10, d6=9, d7=25, cols=16, lines=2)
+
+def viewLCD():
+    """Play the label"""
+    lcd.clear()
+    lcd.message(cWeather1)
+    sleep(2.5)
+    lcd.clear()
+    lcd.message(cWeather2)
+    sleep(2.5)
+    lcd.clear()
+
+def speak(audioString):
+    print(audioString)
+    tts = gTTS(text=audioString, lang='ko')
+    tts.save("audio1.mp3")
+    os.system("omxplayer -o local audio1.mp3")
+ 
+def recordAudio():
+    # Record Audio
+    r = sr.Recognizer()
+    with sr.Microphone(device_index = 2, sample_rate = 44100) as source:
+        print("Say something!")
+        audio = r.listen(source)
+    # Speech recognition using Google Speech Recognition
+    data = ""
+    try:
+        # Uses the default API key
+        # To use another API key: `r.recognize_google(audio, key="GOOGLE_SPEECH_RECOGNITION_API_KEY")`
+        data = r.recognize_google(audio, language = "ko-KR")
+        print("You said: " + data)
+    except sr.UnknownValueError:
+        print("Google Speech Recognition could not understand audio")
+    except sr.RequestError as e:
+        print("Could not request results from Google Speech Recognition service; {0}".format(e))
+ 
+    return data
+ 
+def jarvis(data):
+    if "날씨" in data:
+        weatherInfo = "오늘의 온도는 "+ cTemp+"도이고  날씨는" + cSky + "입니다."
+        viewLCD()
+        speak(weatherInfo)
+        
+    if "시간" in data:
+        speak("현재 시간은 "+ctime()+"년 입니다.")
+ 
+    if "안녕" in data:
+        speak("안녕하세요 주인님")
+ 
+# initialization
+time.sleep(2)
+while 1:
+    data = recordAudio()
+    jarvis(data)
